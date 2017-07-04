37675cceb73fbbf408c09fa1d428d8a64858a01b
Alex <alexhmontgomery@gmail.com>
2017-07-04 12:46:37 -0500

    Add all functionality to site for pulling from Mongo database to display employed and for-hire robots

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const mustache = require('mustache-express')
-const data = require('./data.js')
 const app = express()
 
 app.engine('mustache', mustache())
@@ -8,16 +7,86 @@ app.set('view engine', 'mustache')
 app.set('views', './views')
 app.use(express.static('public'))
 
-app.get('/index', function (req, res) {
-  var users = data.users
-  res.render('index', {users: users})
-})
-
-app.get('/user-:id', function (req, res) {
-  var userID = data.users[parseInt(req.params.id) - 1]
-  res.render('user', {userID: userID})
-})
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
+const url = 'mongodb://localhost:27017/robots'
 
 app.listen(3000, function () {
   console.log('Server ON! Go to Port: 3000')
+})
+
+app.get('/', function (req, res) {
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      throw err
+    } else {
+      console.log('Successfully connected to the database')
+    }
+    const data = require('./data')
+    for (var i = 0; i < data.users.length; i++) {
+      const user = data.users[i]
+      db.collection('users').updateOne(
+      {id: user.id},
+      user,
+      {upsert: true}
+    )
+    }
+    db.collection('users').find()
+    .toArray(function (err, documents) {
+      res.render('index', {
+        users: documents
+      })
+    })
+  })
+})
+
+app.get('/for-hire', function (req, res) {
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      throw err
+    } else {
+      console.log('Successfully connected to the database')
+    }
+    db.collection('users').find({job: null})
+    .toArray(function (err, documents) {
+      console.log(documents)
+      res.render('for-hire', {
+        unemployed: documents
+      })
+    })
+  })
+})
+
+app.get('/employed', function (req, res) {
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      throw err
+    } else {
+      console.log('Successfully connected to the database')
+    }
+    db.collection('users').find({job: {$ne: null}})
+    .toArray(function (err, documents) {
+      console.log(documents)
+      res.render('employed', {
+        employed: documents
+      })
+    })
+  })
+})
+
+app.get('/user-:id', function (req, res) {
+  const userId = parseInt(req.params.id)
+  MongoClient.connect(url, function (err, db) {
+    if (err) {
+      throw err
+    } else {
+      console.log('Successfully connected to the database')
+    }
+    db.collection('users').findOne({id: userId}, function (err, documents) {
+      console.log(documents)
+      res.render('user', {
+        user: documents
+      })
+    })
+  })
 })
